88ea5ba43d5d4726f906524def5b70f7a7caab9b
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-03-01 20:46:43 +0100

    Grouped functions in js

--- js/script.js
+++ js/script.js
@@ -1,37 +1,48 @@
-let formElement = document.querySelector(".js-form");
-let amountElement = document.querySelector(".js-amount");
-let currencyElement = document.querySelector(".js-currency");
-let resultElement = document.querySelector(".js-result");
+{
+    const calculateResult = (amount, currency) => {
 
-let EUR = 0.21;
-let USD = 0.22;
-let CZK = 4.99;
+        let EUR = 0.21;
+        let USD = 0.22;
+        let CZK = 4.99;
 
-formElement.addEventListener("input", () => {
-    let amount = amountElement.value;
-    let currency = currencyElement.value;
-    let result = resultElement.value;
+        switch (currency) {
+            case "EUR":
+                return amount * EUR;
+            case "USD":
+                return amount * USD;
+            case "CZK":
+                return amount * CZK;
+        }
 
-
-    if (amount > Math.pow(100, 9)) {
-        resultElement.innerText = "Za duża wartość";
-        amountElement.classList.add("form__input--wrong");
-        return;
-    }
-    amountElement.classList.remove("form__input--wrong");
-
-    switch (currency) {
-        case "EUR":
-            result = amount * EUR;
-            break;
-        case "USD":
-            result = amount * USD;
-            break;
-        case "CZK":
-            result = amount * CZK;
-            break;
     }
 
-    resultElement.innerText = result.toFixed(2);
-});
+    const onFormInput = () => {
 
+        let amountElement = document.querySelector(".js-amount");
+        let currencyElement = document.querySelector(".js-currency");
+        let resultElement = document.querySelector(".js-result");
+
+        let amount = amountElement.value;
+        let currency = currencyElement.value;
+        let result = calculateResult(amount, currency);
+
+        if (amount > Math.pow(100, 9)) {
+            resultElement.innerText = "Za duża wartość";
+            amountElement.classList.add("form__input--wrong");
+            return;
+        }
+
+        amountElement.classList.remove("form__input--wrong");
+        resultElement.innerText = result.toFixed(2);
+    }
+
+    const init = () => {
+
+        let formElement = document.querySelector(".js-form");
+
+        formElement.addEventListener("input", onFormInput);
+
+    }
+
+    init();
+}
